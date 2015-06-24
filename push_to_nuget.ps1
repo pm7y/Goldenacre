158a86d4205077c6994f12acf318a66f1039e1f2
fallenidol <paul@mcilreavy.com>
2015-06-25 07:09:33 +1000

    nuget stuff

--- push_to_nuget.ps1
+++
@@ -1,9 +0,0 @@
-Get-ChildItem "./pkg" -Filter *.nupkg | `
-Foreach-Object{
-    Write-Host "Pusing $($_.Name)";
-
-    ./.nuget/nuget push $_.Fullname -s https://www.nuget.org
-}
-
-Write-Host "Press any key...";
-Read-Host;
