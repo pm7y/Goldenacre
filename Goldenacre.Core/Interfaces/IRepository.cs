9b5b415562536cb605e81204e6930d30a0a1455f
fallenidol <paul@mcilreavy.com>
2016-03-03 21:36:52 +1000

    added another irepository

--- Goldenacre.Core/Interfaces/IRepository.cs
+++ Goldenacre.Core/Interfaces/IRepository.cs
@@ -5,11 +5,23 @@ using System.Linq.Expressions;
 
 namespace Goldenacre.Core.Interfaces
 {
+    public interface IRepository<T, TKey> : IDisposable where T : class
+    {
+        IQueryable<T> Get(
+            Expression<Func<T, bool>> filter = null);
+
+        T Get(TKey key);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(TKey key);
+        void Delete(T entity);
+        void Save();
+    }
+
     public interface IRepository<T> : IDisposable where T : class
     {
-        IEnumerable<T> Get(
-            Expression<Func<T, bool>> filter = null,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null);
+        IQueryable<T> Get(
+            Expression<Func<T, bool>> filter = null);
 
         T Get(params object[] keys);
         void Insert(T entity);
